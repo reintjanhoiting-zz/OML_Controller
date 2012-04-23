d2f3ba960d63ac5331620c6ecd7314c5e34166be
Reint Jan Hoiting <reintjanhoiting@hotmail.com>
2012-04-23 14:00:22 +0200

    Create project

---
+++ src/oml/controller/OML_ControllerActivity.java
@@ -0,0 +1,13 @@
+package oml.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OML_ControllerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
