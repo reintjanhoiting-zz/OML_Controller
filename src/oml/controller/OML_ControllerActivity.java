a370d584b634bb0ee831bfd10106dafd3e268020
Reint Jan Hoiting <reintjanhoiting@hotmail.com>
2012-04-24 13:28:12 +0200

    Design : Remove Title bar

--- src/oml/controller/OML_ControllerActivity.java
+++ src/oml/controller/OML_ControllerActivity.java
@@ -2,12 +2,14 @@ package oml.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class OML_ControllerActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main);      
     }
 }
